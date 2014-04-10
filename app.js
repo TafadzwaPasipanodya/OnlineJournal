d163c2d21fb1c2319ee532989c6814c35203c13d
Skyler Ng <SkylerNgC@gmail.com>
2014-04-10 14:07:32 -0400

    Added an app.js

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
+// A server with user authentication
 var express = require('express');
+
+// Create a server
 var app = express();
 
-app.get('/',require('./routes/index'));
+// Configure the server
+app.set('view engine', 'ejs');
+app.set('views', __dirname+'/views');
+app.use(express.urlencoded({limit:'1kb'}));
+app.use(express.static(__dirname+'/statics'));
 
-app.get('*', require('./routes/default'));
+// Enable sessions
+// Used to remember user throughout a session (based on cookie)
+// You store a cookie (little files) into the user's browser
+app.use(express.cookieParser());
+// Encypts the cookie
+app.use(express.session({secret:'CS 340 3/6/2014'}));
+
+// Route the requests
+app.get('/', require('./routes/index'));
+app.post('/login', require('./routes/login'));
+app.post('/register', require('./routes/register'));
+app.get('/profile', require('./routes/profile'));
+app.get('/logout', require('./routes/logout'));
+
+// Default route
+app.get('*', function(request,response) {
+    response.send('Nothing to see here!');
+});
+
+// Start the server
 app.listen(8080);
 console.log('Server is up.');
