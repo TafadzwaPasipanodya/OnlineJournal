65ec3e9d0bad9208b1692b06279fba4d09e3de98
Skyler Ng <SkylerNgC@gmail.com>
2014-04-17 22:17:11 -0400

    Editted oneNote.js

---
+++ routes/oneNote.js
@@ -0,0 +1,10 @@
+var notes = require('../models/notes');
+
+module.exports = function(request, response) {
+    var url = request.url;
+    var index = url.lastIndexOf("/");
+    var itemid = url.substring(index+1);
+    notes.retrieveOne(itemid, function(oneNote) {
+        response.render('oneNote', {notes:oneNote});
+    });
+};
