e26dff2c0eb6e0acd8b70cd70b354304c28f877a
Skyler Ng <SkylerNgC@gmail.com>
2014-04-21 17:05:34 -0400

    Fixed the allNotes.js route and the logo.

--- routes/allNotes.js
+++ routes/allNotes.js
@@ -1,7 +1,14 @@
 var note = require('../models/notes');
 
 module.exports = function(request, response) {
-    note.retrieveAll(function(allNotes) {
-        response.render('allNotes', {notes:allNotes});
-    });
+    var username = request.session.username;
+    
+    if (username) {
+        note.retrieveAll(function(allNotes) {
+            response.render('allNotes',{notes:allNotes});
+        });
+    }
+    else {
+        response.redirect('/');
+    }
 };
