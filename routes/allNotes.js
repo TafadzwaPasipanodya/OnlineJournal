e1784e2009ddd2148183a26e45ca69c2b1ab192d
Skyler Ng <SkylerNgC@gmail.com>
2014-04-22 13:31:23 -0400

    Fixed the allNotes.ejs so that it displays the StuInfo.ejs part

--- routes/allNotes.js
+++ routes/allNotes.js
@@ -1,11 +1,17 @@
+// Under the Note Menu: allows user to view all notes
+
 var note = require('../models/notes');
 
 module.exports = function(request, response) {
+    // Get the username from the request session
     var username = request.session.username;
     
+    // If logged in, then...
     if (username) {
+        
+        // Render all notes from database
         note.retrieveAll(function(allNotes) {
-            response.render('allNotes',{notes:allNotes});
+            response.render('allNotes', {notes:allNotes, username:username});
         });
     }
     else {
--- routes/oneNote.js
+++ routes/oneNote.js
@@ -1,10 +1,23 @@
+// Under the Note Menu: allows user to view all notes
+
 var notes = require('../models/notes');
 
 module.exports = function(request, response) {
+    // Get the username from the request session
+    var username = request.session.username;
+    
     var url = request.url;
     var index = url.lastIndexOf("/");
+    
+    // This is the '._id' from the database
     var itemid = url.substring(index+1);
-    notes.retrieveOne(itemid, function(oneNote) {
-        response.render('oneNote', {note:oneNote});
-    });
+    
+    if (username) {
+        notes.retrieveOne(itemid, function(oneNote) {
+         response.render('oneNote', {note:oneNote});
+        });
+    }
+    else {
+        reponse.redirect('/');
+    }
 };
