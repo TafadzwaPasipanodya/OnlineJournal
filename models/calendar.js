c06faabfe880525ecd15ec8dd354f22fed1838c3
Tafadzwa Pasipanodya <tmpasipanodya@gmail.com>
2014-04-24 13:45:36 -0400

    Added calendar functionality. Calendar now functional in the sence that it displays the current month

--- models/calendar.js
+++ models/calendar.js
@@ -1,3 +1,24 @@
-// A model for a note collection
-var mongojs = require('mongojs');
+// function to get the total number of days in a given month
+function numberOfDays(year, month) {
+    var d = new Date(year, month, 0);
+    return d.getDate();
+}
 
+// function to get all the days in a given month
+module.exports.daysInMonths = function (year, month, callback){
+    var days = ["","","","","","","",
+                "","","","","","","",
+                "","","","","","","",
+                "","","","","","","",
+                "","","","","","","",
+                "","","","","","",""]
+    var d = new Date(year, month, 1);
+    var n = d.getDay();
+
+    var m = numberOfDays(year, month);
+
+    for (var i = n; i<n+m; i++) {
+        days[i] = i-(n-1);
+    }
+        callback(days);
+}
--- routes/calendarTab.js
+++ routes/calendarTab.js
@@ -1,13 +1,15 @@
 // Note page: Create, edit, save, view, or delete notes
 
-var users = require('../models/calendar');
+var calendar = require('../models/calendar');
 
 module.exports = function(request,response) {
     var username = request.session.username;
     
     // If logged in, go to calendar
     if (username) {
-        response.render('calendar', {username:username});
+        calendar.daysInMonths(2837, 7, function(days){
+            response.render('calendar',{month:days,username:username})
+            });
     }
     
     // Sends to another route
