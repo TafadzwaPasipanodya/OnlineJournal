b3d87167e3b51faae9536f6788397825a6faac7d
Skyler Ng <SkylerNgC@gmail.com>
2014-05-08 09:48:48 -0400

    Fixed a bug

--- models/notes.js
+++ models/notes.js
@@ -14,8 +14,6 @@ module.exports.create = function(name, content, callback) {
 
 // Edit an existing note
 module.exports.edit = function(noteid, content, callback) {
-    // Receive the content from the textarea
-    // var content = request.body.content;
     
     db.notes.findOne({_id:mongojs.ObjectId(noteid)}, function(error, note) {
         if (error) throw error;
@@ -51,8 +49,10 @@ module.exports.retrieveAll = function(callback) {
 
 // View one note from database
 module.exports.retrieveOne = function(noteid, callback) {
+    console.log(noteid);
     db.notes.findOne({_id:mongojs.ObjectId(noteid)}, function(error, note) {
         if (error) throw error;
+        console.log(note);
         callback(note);
     });
 };
